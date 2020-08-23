bff8eafa3f5074550fc9f492954554856d0e8c33
he-guang-wen <1548319928@qq.com>
2020-08-23 10:56:29 +0800

    2

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -80,7 +80,8 @@ export default {
   mounted() {
     // console.log(this.$site.base,'this.$sitethis.$site')
     if (this.$page.path == "/") {
-      window.location.href = this.origin + "guide/quickstart.html";
+      // window.location.href = this.origin + "guide/quickstart.html";
+      // return
     }
 
     window.addEventListener(
